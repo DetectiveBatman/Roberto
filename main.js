f4baadbbfadac080d95534667b3305491d634e54
DetectiveBatman <sinamedic1@gmail.com>
2019-09-22 14:39:40 +0330

    created index file

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const chalk = require('chalk');
 const app = express();
 const args = process.argv.splice(2);
 const port = args[0] || 8546;
+
+app.get('/panel', (req, res, next) => {
+  res.sendFile(__dirname + '/view/index.html');
+});
+
+
+app.listen(port, () => {
+  console.log(chalk.green.bold('[*] ') + chalk.green.bold('Listening on port:') + ' ' + chalk.red.bold(port));
+});
