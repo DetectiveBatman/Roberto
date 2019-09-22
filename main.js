5987d09e4cd4ea01270eda02b3e419dd67f2faf8
DetectiveBatman <sinamedic1@gmail.com>
2019-09-22 16:53:23 +0330

    it now manages all of lib requests!

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ const app = express();
 const args = process.argv.splice(2);
 const port = args[0] || 8546;
 
+
 app.get('/panel', (req, res, next) => {
   res.sendFile(__dirname + '/view/index.html');
 });
 
-app.get('/favicon', (req, res, next) => {
+app.get('/panel/favicon', (req, res, next) => {
   res.sendFile(__dirname + '/lib/assets/favicon.png');
+});
+
+app.get('/panel/lib/*', (req, res, next) => {
+  let path = __dirname + req.url.substr(6);
+  res.sendFile(path)
 });
 
 app.listen(port, () => {
